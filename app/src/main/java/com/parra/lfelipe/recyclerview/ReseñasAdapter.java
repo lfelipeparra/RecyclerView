9759ae11334d4fca515b4c75556896d09b1a55b9
Felipe <lfelipe.parra@udea.edu.co>
2017-11-07 13:59:31 -0500

    Version 4.0

--- app/src/main/java/com/parra/lfelipe/recyclerview/ReseñasAdapter.java
+++ app/src/main/java/com/parra/lfelipe/recyclerview/ReseñasAdapter.java
@@ -1,10 +1,34 @@
 package com.parra.lfelipe.recyclerview;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by Manuela on 7/11/2017.
  */
 
-public class ReseñasAdapter  {
+public class ReseñasAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
 
+    @Override
+    public RecyclerAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
 
+    @Override
+    public void onBindViewHolder(RecyclerAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
